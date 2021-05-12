b7e86f19b2c5d9513a657b610f30cee7b7f6bbfa
Rahul Rachh <rahulrachh3@gmail.com>
2021-05-12 12:57:29 -0700

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -2,7 +2,7 @@ import var
 import telebot
 import yfinance as yf
 
-# created a var.py file where I stored the apikey and importing that from var.py file
+# stored the api key in var file and imported that here.
 API_KEY = var.api_key
 bot = telebot.TeleBot(API_KEY)
 
@@ -44,17 +44,28 @@ def get_stocks(message):
     bot.send_message(message.chat.id, response)
         # stock_request(message)
 
-    def stock_request(message):
-        request = message.text.split()
-        if len(request)<2 or request[0].lower() not in "price":
-            return False
-        else:
-            return True
+def stock_request(message):
+    request = message.text.split()
+    if len(request)<2 or request[0].lower() not in "price":
+        return False
+    else:
+        return True
 
 
-    @bot.message_handler(func=stock_request)
-    def send_price(message):
-        pass
+@bot.message_handler(func=stock_request)
+def send_price(message):
+    request = message.text.split()[1]
+    comapanyName = yf.Ticker(request).info['longName']
+    data = yf.download(tickers=request, period='5m', interval='1m')
+    if data.size>0:
+        data = data.reset_index()
+        data['format_date'] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
+        data.set_index('format_date', inplace=True)
+        print(data.to_string())
+        bot.send_message(message.chat.id, f'The {comapanyName} stock price is:\n'+data['Close'].to_string(header=False))
+    else:
+        bot.send_message(message.chat.id, "No Data!!!!!!!!!!!")
+    pass
 
 
 
