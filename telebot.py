4114b7092b2937b851f2eee6f0665a82a1b615f1
Rahul Rachh <rahulrachh3@gmail.com>
2021-07-13 16:36:32 -0700

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -2,7 +2,7 @@ import telebot
 import yfinance as yf
 
 # stored the api key in var file and imported that here.
-API_KEY = "1816068848:AAGsW_HLWzBKhylQ4lFAahMqbksvt5r4h0s"
+API_KEY = API_KEY
 bot = telebot.TeleBot(API_KEY)
 
 # runs by sending '/greet' in the bot chat
